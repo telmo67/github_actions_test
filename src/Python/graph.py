a61ee705d23ce706e7337df75907dc8441d73333
Telmo Alves <telmomiguel31@gmail.com>
2026-02-17 22:45:47 +0000

    fix(project): Deal With Depedencies and Add Docstring Comments
    Signed-off-by: Telmo Alves <telmomiguel31@gmail.com>

--- src/Python/graph.py
+++ src/Python/graph.py
@@ -1,10 +1,16 @@
+"""
+Simple sine wave plotting module
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def sin(x):
+    """Retun the sine of x"""
     return np.sin(x)
 
 def plot(t1, t2, output_file="./sine.png"):
+    """Plot sine wave between t1 and t2 and save to file."""
     t = np.linspace(t1, t2, 1000)
     plt.plot(t, sin(t))
     plt.title("Sine Wave")
