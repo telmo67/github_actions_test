283da84aea954037c7776c4027ba254b132f1b48
Telmo Alves <telmomiguel31@gmail.com>
2026-03-05 12:03:18 +0000

    feat(c-lint): Add C Static Analysis
    Signed-off-by: Telmo Alves <telmomiguel31@gmail.com>

--- src/C/home.c
+++ src/C/home.c
@@ -6,12 +6,16 @@
 
 int main() {
 
-  home_t *myHouse;
+  home_t *myHouse = malloc(sizeof(home_t));
+  if (myHouse == NULL) {
+    return 1;
+  }
   char myCity[] = "Guimarães";
   char myCountry[] = "Portugal";
 
   myHouse->city = malloc(sizeof(myCity));
   if (myHouse->city == NULL) {
+    free(myHouse);
     return 1;
   }
   strcpy(myHouse->city, myCity);
@@ -19,11 +23,13 @@ int main() {
   myHouse->country = malloc(sizeof(myCountry));
   if (myHouse->country == NULL) {
     free(myHouse->city);
+    free(myHouse);
     return 1;
   }
   strcpy(myHouse->country, myCountry);
 
   free(myHouse->country);
   free(myHouse->city);
+  free(myHouse);
   return 0;
 }
